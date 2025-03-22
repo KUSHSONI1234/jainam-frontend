65c99e78cc9e464f785cdad325b13a057f883930
kush6 <kush60513@gmail.com>
2025-03-22 14:50:29 +0530

    cha nge

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,52 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  formData = {
+  loginData = {
     email: '',
     password: ''
   };
 
-  showPassword = false;
-
-  passwordValidations = {
-    minLength: false,
-    hasUppercase: false,
-    hasLowercase: false,
-    hasNumber: false,
-    hasSpecialChar: false
-  };
-
-  onPasswordInput() {
-    const password = this.formData.password;
-    this.passwordValidations.minLength = password.length >= 8;
-    this.passwordValidations.hasUppercase = /[A-Z]/.test(password);
-    this.passwordValidations.hasLowercase = /[a-z]/.test(password);
-    this.passwordValidations.hasNumber = /\d/.test(password);
-    this.passwordValidations.hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
-  }
-
-  onSubmit(form: any) {
-    if (form.invalid) {
-      alert("Please fill in all required fields correctly.");
-      return;
-    }
-
-    console.log("Login Successful", this.formData);
-    alert("Login successful!");
-
-    // Reset the form
-    this.formData = {
-      email: '',
-      password: ''
-    };
-    form.resetForm();
+  onLogin() {
+    console.log('Login Data:', this.loginData);
+    alert('Login Successful!');
   }
 }
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -3,62 +3,25 @@ import { Component } from '@angular/core';
 // import { FormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { FooterComponent } from "../footer/footer.component";
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-sign-up',
-  imports: [FormsModule, CommonModule, FooterComponent],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  formData = {
-    firstName: '',
-    lastName: '',
+  signupData = {
+    name: '',
     email: '',
-    password: '',
-    confirmPassword: ''
+    phone: '',
+    password: ''
   };
 
-  showPassword = false;
-  showConfirmPassword = false;
-
-  passwordValidations = {
-    minLength: false,
-    hasUppercase: false,
-    hasLowercase: false,
-    hasNumber: false,
-    hasSpecialChar: false
-  };
-
-  onPasswordInput() {
-    const password = this.formData.password;
-    this.passwordValidations.minLength = password.length >= 8;
-    this.passwordValidations.hasUppercase = /[A-Z]/.test(password);
-    this.passwordValidations.hasLowercase = /[a-z]/.test(password);
-    this.passwordValidations.hasNumber = /\d/.test(password);
-    this.passwordValidations.hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  onSignup() {
+    console.log('Signup Data:', this.signupData);
+    alert('Signup Successful!');
   }
-
-  onSubmit(form: any) {
-    if (this.formData.password !== this.formData.confirmPassword) {
-      alert("Passwords do not match!");
-      return;
-    }
-
-    console.log("Form Submitted", this.formData);
-    alert("Sign-up successful!");
-
-    // Reset the form
-    this.formData = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-    };
-    form.resetForm();
-  }
-
-
 }
