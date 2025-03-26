960fd4f9bd7cb87ff0dd316f0e1f28e06eb5af46
kush6 <kush60513@gmail.com>
2025-03-27 00:10:16 +0530

    changes

--- src/app/components/environment.ts
+++ src/app/components/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: window.location.hostname !== 'localhost',   // true for production, false for local
     apiUrl: window.location.hostname === 'localhost'
-      ? 'http://localhost:3000/api'       // Local environment
-      : 'https://your-production-url.com/api'    // Production environment
+      ? 'http://localhost:4200/api'       // Local environment
+      : 'https://jainam-backend.onrender.com'    // Production environment
   };
   
